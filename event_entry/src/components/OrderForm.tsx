5211242ed967746718c353bf72bf895fa00d26cc
Brendan Woo <woo.brendan@gmail.com>
2024-03-01 15:52:47 -0800

    add usestate for order, then comments for orders incoming. import tickettype component and create func select tyicket type

--- event_entry/src/components/OrderForm.tsx
+++ event_entry/src/components/OrderForm.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import TicketType from "./TicketType";
 const OrderForm: React.FC = () => {
-    return <section className="order_form_container">{/* select Order type (event, lumirank, bronze) */}</section>;
+    // order 1: type event
+    //order2 : type event
+    //order3: lumirank rental
+    const [order, setOrder] = useState({
+        order1: { type: "" },
+    });
+
+    const selectTicketType = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOrder((prev) => ({
+            ...prev,
+            order1: { type: e.target.value },
+        }));
+    };
+
+    return (
+        <section className="order_form_container">
+            {/* select Order type (event, lumirank, bronze) */}
+            <TicketType handleChange={selectTicketType} />
+        </section>
+    );
 };
 
 export default OrderForm;
