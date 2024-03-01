fcec84d9fbfb3cceb22df4da99588ee12ed52af7
Brendan Woo <woo.brendan@gmail.com>
2024-03-01 11:29:27 -0800

    create new compoent for starting a new order

---
+++ event_entry/src/components/NewOrder.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const NewOrder: React.FC = () => {
+    return (
+        <div>
+            <Button />
+        </div>
+    );
+};
+
+export default NewOrder;
