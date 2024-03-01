c84a2ff256edd26416368ff8cb7682b90843f7c5
Brendan Woo <woo.brendan@gmail.com>
2024-03-01 11:57:24 -0800

    add new h1 message and h3 sub message. add button styling and message on button

--- event_entry/src/components/NewOrder.tsx
+++ event_entry/src/components/NewOrder.tsx
@@ -2,8 +2,12 @@ import { Button } from "@mui/material";
 
 const NewOrder: React.FC = () => {
     return (
-        <div>
-            <Button />
+        <div className="new_order_container">
+            <h1>2025 SRO Motorsports America Season</h1>
+            <h3>Welcome to the event entry website for SROMA. To begin order, please click below</h3>
+            <Button variant="contained" color="error">
+                Start
+            </Button>
         </div>
     );
 };
