d74b7aefbb3bc06f13c0f3fb072c1c13f76ebcf0
Brendan Woo <woo.brendan@gmail.com>
2024-03-04 12:20:05 -0800

    create props interfae for next func, and pass into deconstruction and pass to button

--- event_entry/src/components/NewOrder.tsx
+++ event_entry/src/components/NewOrder.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@mui/material";
 
-const NewOrder: React.FC = () => {
+interface Props {
+    next: () => void;
+}
+
+const NewOrder: React.FC<Props> = ({ next }) => {
     return (
         <div className="new_order_container">
             <h1>2025 SRO Motorsports America Season</h1>
             <h3>Welcome to the event entry website for SROMA. To begin order, please click below</h3>
-            <Button variant="contained" color="error">
+            <Button variant="contained" color="error" onClick={() => next()}>
                 Start
             </Button>
         </div>
