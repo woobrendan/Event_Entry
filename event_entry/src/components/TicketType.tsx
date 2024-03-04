c860f23ae94fe352d9d0d797124abcb0738b975b
Brendan Woo <woo.brendan@gmail.com>
2024-03-04 13:04:16 -0800

    add wrapper div for radio inputs. import compNav props, and add button for nav

--- event_entry/src/components/TicketType.tsx
+++ event_entry/src/components/TicketType.tsx
@@ -1,37 +1,46 @@
+import { Button } from "@mui/material";
+
 interface Props {
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    compNav: (val: string) => void;
 }
 
-const TicketType: React.FC<Props> = ({ handleChange }) => {
+const TicketType: React.FC<Props> = ({ handleChange, compNav }) => {
     return (
         <section className="ticket_type">
-            <label>
-                <input
-                    type="radio"
-                    value="option1"
-                    //checked={selectedOption === "option1"}
-                    onChange={(e) => handleChange(e)}
-                />
-                Event Entry
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    value="option1"
-                    //checked={selectedOption === "option1"}
-                    onChange={(e) => handleChange(e)}
-                />
-                Lumirank Rental
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    value="option1"
-                    //checked={selectedOption === "option1"}
-                    onChange={(e) => handleChange(e)}
-                />
-                Bronze Test Session
-            </label>
+            <div className="ticket_type__radios">
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    Event Entry
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    Lumirank Rental
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    Bronze Test Session
+                </label>
+            </div>
+
+            <Button variant="contained" color="error" onClick={() => compNav("next")}>
+                Next
+            </Button>
         </section>
     );
 };
