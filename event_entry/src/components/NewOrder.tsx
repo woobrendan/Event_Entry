ce8e8205bc6304861d8c7773659945ec92e30fcc
Brendan Woo <woo.brendan@gmail.com>
2024-03-04 13:04:45 -0800

    add string param to compNav funnc and pass to button

--- event_entry/src/components/NewOrder.tsx
+++ event_entry/src/components/NewOrder.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@mui/material";
 
 interface Props {
-    next: () => void;
+    compNav: (val: string) => void;
 }
 
-const NewOrder: React.FC<Props> = ({ next }) => {
+const NewOrder: React.FC<Props> = ({ compNav }) => {
     return (
         <div className="new_order_container">
             <h1>2025 SRO Motorsports America Season</h1>
             <h3>Welcome to the event entry website for SROMA. To begin order, please click below</h3>
-            <Button variant="contained" color="error" onClick={() => next()}>
+            <Button variant="contained" color="error" onClick={() => compNav("next")}>
                 Start
             </Button>
         </div>
