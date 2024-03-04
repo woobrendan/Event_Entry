b7545f399b71123a73e52d84f6751ce2736ddddb
Brendan Woo <woo.brendan@gmail.com>
2024-03-04 13:17:35 -0800

    add radio buttons for each series, add string val to prop compnav

--- event_entry/src/components/event_entry/SeriesSelect.tsx
+++ event_entry/src/components/event_entry/SeriesSelect.tsx
@@ -1,9 +1,59 @@
 import { Button } from "@mui/material";
 
-const SeriesSelect: React.FC = () => {
+interface Props {
+    compNav: (val: string) => void;
+}
+
+const SeriesSelect: React.FC<Props> = ({ compNav }) => {
     return (
-        <section>
-            {/* select series */}
+        <section className="series_select">
+            <div className="series_select__radios">
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        //onChange={(e) => handleChange(e)}
+                    />
+                    Fanatec GT World Challenge America
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        //onChange={(e) => handleChange(e)}
+                    />
+                    Pirelli GT4 America
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        //onChange={(e) => handleChange(e)}
+                    />
+                    GT America
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        //onChange={(e) => handleChange(e)}
+                    />
+                    TC America
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="option1"
+                        //checked={selectedOption === "option1"}
+                        //onChange={(e) => handleChange(e)}
+                    />
+                    Toyota GR Cup
+                </label>
+            </div>
             <div className="comp_nav_buttons">
                 <Button variant="outlined" color="error" onClick={() => compNav("back")}>
                     Back
