04a471d2e01063a2f1b0a78e70131ac0c8347742
Brendan Woo <woo.brendan@gmail.com>
2024-03-05 11:22:37 -0800

    to each input, add name, change value to series. uncomment onchange to be setKeyval. add setkeyval to props

--- event_entry/src/components/event_entry/SeriesSelect.tsx
+++ event_entry/src/components/event_entry/SeriesSelect.tsx
@@ -2,27 +2,30 @@ import { Button } from "@mui/material";
 
 interface Props {
     compNav: (val: string) => void;
+    setKeyVal: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SeriesSelect: React.FC<Props> = ({ compNav }) => {
+const SeriesSelect: React.FC<Props> = ({ compNav, setKeyVal }) => {
     return (
         <section className="series_select">
             <div className="series_select__radios">
                 <label>
                     <input
                         type="radio"
-                        value="option1"
+                        value="Fanatec GT World Challenge America"
+                        name="series"
                         //checked={selectedOption === "option1"}
-                        //onChange={(e) => handleChange(e)}
+                        onChange={(e) => setKeyVal(e)}
                     />
                     Fanatec GT World Challenge America
                 </label>
                 <label>
                     <input
                         type="radio"
-                        value="option1"
+                        value="Pirelli GT4 America"
+                        name="series"
                         //checked={selectedOption === "option1"}
-                        //onChange={(e) => handleChange(e)}
+                        onChange={(e) => setKeyVal(e)}
                     />
                     Pirelli GT4 America
                 </label>
@@ -30,26 +33,29 @@ const SeriesSelect: React.FC<Props> = ({ compNav }) => {
                     <input
                         type="radio"
                         value="option1"
+                        name="series"
                         //checked={selectedOption === "option1"}
-                        //onChange={(e) => handleChange(e)}
+                        onChange={(e) => setKeyVal(e)}
                     />
                     GT America
                 </label>
                 <label>
                     <input
                         type="radio"
-                        value="option1"
+                        value=" TC America"
+                        name="series"
                         //checked={selectedOption === "option1"}
-                        //onChange={(e) => handleChange(e)}
+                        onChange={(e) => setKeyVal(e)}
                     />
                     TC America
                 </label>
                 <label>
                     <input
                         type="radio"
-                        value="option1"
+                        value="Toyota GR Cup"
+                        name="series"
                         //checked={selectedOption === "option1"}
-                        //onChange={(e) => handleChange(e)}
+                        onChange={(e) => setKeyVal(e)}
                     />
                     Toyota GR Cup
                 </label>
