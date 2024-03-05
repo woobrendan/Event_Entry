bdee183d46735cc327019fd7dfcc4d7c6c2ac8c7
Brendan Woo <woo.brendan@gmail.com>
2024-03-05 11:28:13 -0800

    coment out order1 from state, remove selecttickettpye funnc and replace setkeyval, pass to each component

--- event_entry/src/components/OrderForm.tsx
+++ event_entry/src/components/OrderForm.tsx
@@ -8,30 +8,28 @@ import SeriesSelect from "./event_entry/SeriesSelect";
 import "../styles/orderForm.scss";
 
 const OrderForm: React.FC = () => {
-    // order 1: type event
-    //order2 : type event
-    //order3: lumirank rental
+    // each order will indicate type, at end add to redux cart
     const [order, setOrder] = useState({
-        order1: { type: "" },
+        //order1: { type: "" },
     });
 
     const [currentComp, setCurrentComp] = useState(0);
-
-    const selectTicketType = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setOrder((prev) => ({
-            ...prev,
-            order1: { type: e.target.value },
-        }));
-    };
 
     const componentNavigation = (val: string): void => {
         val === "next" ? setCurrentComp(currentComp + 1) : setCurrentComp(currentComp - 1);
     };
 
+    const setKeyVal = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOrder((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
     const components = [
         <NewOrder compNav={componentNavigation} />,
-        <TicketType handleChange={selectTicketType} compNav={componentNavigation} />,
-        <SeriesSelect compNav={componentNavigation} />,
+        <TicketType setKeyVal={setKeyVal} compNav={componentNavigation} />,
+        <SeriesSelect setKeyVal={setKeyVal} compNav={componentNavigation} />,
         //next comp
     ];
 
