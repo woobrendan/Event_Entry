bbd9cf8918f1f6466c3e022894c9cfe827844d9f
Brendan Woo <woo.brendan@gmail.com>
2024-03-06 13:22:02 -0800

    create ticket types array, and map over tickets to return labels and replace inside div

--- event_entry/src/components/TicketType.tsx
+++ event_entry/src/components/TicketType.tsx
@@ -6,41 +6,29 @@ interface Props {
 }
 
 const TicketType: React.FC<Props> = ({ setKeyVal, compNav }) => {
-    return (
-        <section className="ticket_type">
-            <div className="ticket_type__radios">
-                <label>
-                    <input
-                        type="radio"
-                        value="event"
-                        name="type"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Event Entry
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="lumirank"
-                        name="type"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Lumirank Rental
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="bronze"
-                        name="type"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Bronze Test Session
-                </label>
-            </div>
+    const ticketTypes = [
+        { value: "event", label: "Event Entry" },
+        { value: "lumirank", label: "Lumirank Rental" },
+        { value: "bronzeTest", label: "Bronze Test Session" },
+    ];
 
+    const mappedTickets = ticketTypes.map((ticket, index) => {
+        return (
+            <label key={index}>
+                <input
+                    type="radio"
+                    value={ticket.value}
+                    name="type"
+                    //checked={selectedOption === "option1"}
+                    onChange={(e) => setKeyVal(e)}
+                />
+                {ticket.label}
+            </label>
+        );
+    });
+    return (
+        <section className="ticket select">
+            <div className="select__radios">{mappedTickets}</div>
             <Button variant="contained" color="error" onClick={() => compNav("next")}>
                 Next
             </Button>
