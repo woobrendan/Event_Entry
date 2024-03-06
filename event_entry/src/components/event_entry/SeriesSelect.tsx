ec641dd91333f20c096b6def2d4829001e893676
Brendan Woo <woo.brendan@gmail.com>
2024-03-06 13:26:12 -0800

    create array of series then map over creating labels, replace each hardcoded label with mappedSeries

--- event_entry/src/components/event_entry/SeriesSelect.tsx
+++ event_entry/src/components/event_entry/SeriesSelect.tsx
@@ -6,60 +6,31 @@ interface Props {
 }
 
 const SeriesSelect: React.FC<Props> = ({ compNav, setKeyVal }) => {
+    const series = [
+        "Fanatec GT World Challenge America",
+        "Pirelli GT4 America",
+        "GT America",
+        "TC America",
+        "Toyota GR Cup",
+    ];
+
+    const mappedSeries = series.map((val, index) => {
+        return (
+            <label key={index}>
+                <input
+                    type="radio"
+                    value={val}
+                    name="series"
+                    //checked={selectedOption === "option1"}
+                    onChange={(e) => setKeyVal(e)}
+                />
+                {val}
+            </label>
+        );
+    });
     return (
         <section className="series select">
-            <div className="select__radios">
-                <label>
-                    <input
-                        type="radio"
-                        value="Fanatec GT World Challenge America"
-                        name="series"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Fanatec GT World Challenge America
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="Pirelli GT4 America"
-                        name="series"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Pirelli GT4 America
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="option1"
-                        name="series"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    GT America
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value=" TC America"
-                        name="series"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    TC America
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        value="Toyota GR Cup"
-                        name="series"
-                        //checked={selectedOption === "option1"}
-                        onChange={(e) => setKeyVal(e)}
-                    />
-                    Toyota GR Cup
-                </label>
-            </div>
+            <div className="select__radios">{mappedSeries}</div>
             <BackNextButtons compNav={compNav} />
         </section>
     );
