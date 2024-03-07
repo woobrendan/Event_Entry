97a334f0b99a6321ab5f20f9124c49292e13f3b5
Brendan Woo <woo.brendan@gmail.com>
2024-03-07 11:28:08 -0800

    change handlechange to handle input for type input elemtn, create handle selectl for select elements. create select element and set defatul option

--- event_entry/src/components/event_entry/TeamNumber.tsx
+++ event_entry/src/components/event_entry/TeamNumber.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { SetAndNav } from "../../models/props";
+import { teams } from "../../seeds/teams";
 
 const TeamNumber: React.FC<SetAndNav> = ({ compNav, setKeyVal }) => {
     const [details, setDetails] = useState({
         number: "",
         team: "",
     });
-    // input field for number
-    //dropdown for team name
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDetails((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setDetails((prev) => ({
             ...prev,
             [e.target.name]: e.target.value,
@@ -17,9 +23,15 @@ const TeamNumber: React.FC<SetAndNav> = ({ compNav, setKeyVal }) => {
     };
 
     return (
-        <section>
+        <section className="team_info input">
             <label>Car Number:</label>
-            <input type="text" value={details.number} name="number" onChange={handleChange} />
+            <input type="number" value={details.number} name="number" onInput={handleInput} />
+            <select name="team" value={details.team} onInput={handleSelect}>
+                <option value="" disabled>
+                    Select Team
+                </option>
+                {/*{teams.map((team,index))}*/}
+            </select>
         </section>
     );
 };
