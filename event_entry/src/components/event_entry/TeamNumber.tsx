ce9f68e24a67365e42022a268347c24b15642ab7
Brendan Woo <woo.brendan@gmail.com>
2024-03-07 10:33:17 -0800

    create state for details, seting umber and team. create func handle change. pass to input for text number

--- event_entry/src/components/event_entry/TeamNumber.tsx
+++ event_entry/src/components/event_entry/TeamNumber.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const TeamNumber: React.FC = () => {
+    const [details, setDetails] = useState({
+        number: "",
+        team: "",
+    });
     // input field for number
     //dropdown for team name
-    return <></>;
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDetails((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    return (
+        <section>
+            <label>Car Number:</label>
+            <input type="text" value={details.number} name="number" onChange={handleChange} />
+        </section>
+    );
 };
 
 export default TeamNumber;
