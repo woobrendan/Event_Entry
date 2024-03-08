4f67ce5b6b7dcfc87d741811bc2ff01893dbcf0a
Brendan Woo <woo.brendan@gmail.com>
2024-03-08 11:50:17 -0800

    change handle input element key to be handleformelement and update to use generics to extend input and select elements

--- event_entry/src/models/props.ts
+++ event_entry/src/models/props.ts
@@ -1,4 +1,4 @@
 export interface SetAndNav {
     compNav: (val: string) => void;
-    handleInputElement: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleFormElement: <T extends HTMLInputElement | HTMLSelectElement>(e: React.ChangeEvent<T>) => void;
 }
