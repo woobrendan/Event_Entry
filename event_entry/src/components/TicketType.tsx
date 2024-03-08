a8cac7fdacbfd359eabf81ccfb386aadd128035b
Brendan Woo <woo.brendan@gmail.com>
2024-03-08 13:47:58 -0500

    create new interface to handle props of handleboxclick. create box tickets variable and replace mappedtickets

--- event_entry/src/components/TicketType.tsx
+++ event_entry/src/components/TicketType.tsx
@@ -2,7 +2,12 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 import { SetAndNav } from "../models/props";
 
-const TicketType: React.FC<SetAndNav> = ({ handleFormElement, compNav }) => {
+interface Props {
+    handleBoxClick: (name: string, val: string) => void;
+    compNav: (val: string) => void;
+}
+
+const TicketType: React.FC<Props> = ({ handleBoxClick, compNav }) => {
     const [selected, setSelected] = useState("");
 
     const ticketTypes = [
@@ -11,28 +16,45 @@ const TicketType: React.FC<SetAndNav> = ({ handleFormElement, compNav }) => {
         { value: "bronzeTest", label: "Bronze Test Session" },
     ];
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSelected(e.target.value);
-        handleFormElement(e);
+    //const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //    setSelected(e.target.value);
+    //    handleFormElement(e);
+    //};
+
+    const handleClick = (val) => {
+        setSelected(val);
     };
 
-    const mappedTickets = ticketTypes.map((ticket, index) => {
+    //const mappedTickets = ticketTypes.map((ticket, index) => {
+    //    return (
+    //        <label key={index}>
+    //            <input
+    //                type="radio"
+    //                value={ticket.value}
+    //                name="type"
+    //                checked={selected === ticket.value}
+    //                onChange={(e) => handleChange(e)}
+    //            />
+    //            {ticket.label}
+    //        </label>
+    //    );
+    //});
+
+    const boxTickets = ticketTypes.map((ticket, index) => {
         return (
-            <label key={index}>
-                <input
-                    type="radio"
-                    value={ticket.value}
-                    name="type"
-                    checked={selected === ticket.value}
-                    onChange={(e) => handleChange(e)}
-                />
+            <div
+                className={`ticket click_box ${selected === ticket.value ? "selected" : ""}`}
+                onClick={() => handleClick(ticket.value)}
+                key={index}
+            >
                 {ticket.label}
-            </label>
+            </div>
         );
     });
+
     return (
         <section className="ticket select">
-            <div className="select__radios">{mappedTickets}</div>
+            <div className="select__radios">{boxTickets}</div>
             <Button variant="contained" color="error" onClick={() => compNav("next")}>
                 Next
             </Button>
