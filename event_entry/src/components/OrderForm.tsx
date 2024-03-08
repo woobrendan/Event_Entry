87d7fb3e2288e4d2f460ab550b5858fc52af244f
Brendan Woo <woo.brendan@gmail.com>
2024-03-08 11:39:43 -0800

    import team number component. change handlechange to be handleinputelement, update each prop

--- event_entry/src/components/OrderForm.tsx
+++ event_entry/src/components/OrderForm.tsx
@@ -5,6 +5,7 @@ import TicketType from "./TicketType";
 import NewOrder from "./NewOrder";
 import SeriesSelect from "./event_entry/SeriesSelect";
 import EventSelect from "./event_entry/EventSelect";
+import TeamNumber from "./event_entry/TeamNumber";
 
 import "../styles/orderForm.scss";
 
@@ -20,7 +21,7 @@ const OrderForm: React.FC = () => {
         val === "next" ? setCurrentComp(currentComp + 1) : setCurrentComp(currentComp - 1);
     };
 
-    const setKeyVal = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputElement = (e: React.ChangeEvent<HTMLInputElement>) => {
         setOrder((prev) => ({
             ...prev,
             [e.target.name]: e.target.value,
@@ -29,10 +30,10 @@ const OrderForm: React.FC = () => {
 
     const components = [
         <NewOrder compNav={componentNavigation} />,
-        <TicketType setKeyVal={setKeyVal} compNav={componentNavigation} />,
-        <EventSelect setKeyVal={setKeyVal} compNav={componentNavigation} />,
-        <SeriesSelect setKeyVal={setKeyVal} compNav={componentNavigation} />,
-        //next comp
+        <TicketType handleInputElement={handleInputElement} compNav={componentNavigation} />,
+        <EventSelect handleInputElement={handleInputElement} compNav={componentNavigation} />,
+        <SeriesSelect handleInputElement={handleInputElement} compNav={componentNavigation} />,
+        <TeamNumber handleInputElement={handleInputElement} compNav={componentNavigation} />,
     ];
 
     const CurrentComponent = components[currentComp];
