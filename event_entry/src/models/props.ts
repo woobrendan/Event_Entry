4436799730d98d6d3331159a38457db306f63679
Brendan Woo <woo.brendan@gmail.com>
2024-03-08 11:39:59 -0800

    change name of setkeyval to be handleinputelement

--- event_entry/src/models/props.ts
+++ event_entry/src/models/props.ts
@@ -1,4 +1,4 @@
 export interface SetAndNav {
     compNav: (val: string) => void;
-    setKeyVal: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleInputElement: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
