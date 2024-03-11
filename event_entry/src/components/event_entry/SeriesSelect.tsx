b2da95d4684de01f87fe6dfc6f394be58c402b79
Brendan Woo <woo.brendan@gmail.com>
2024-03-11 15:07:26 -0400

    create boxseries val with clickable divs. replace mappedseries in select_radios

--- event_entry/src/components/event_entry/SeriesSelect.tsx
+++ event_entry/src/components/event_entry/SeriesSelect.tsx
@@ -13,28 +13,44 @@ const SeriesSelect: React.FC<SetAndNav> = ({ compNav, handleFormElement }) => {
         "Toyota GR Cup",
     ];
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSelected(e.target.value);
-        handleFormElement(e);
+    //const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //    setSelected(e.target.value);
+    //    handleFormElement(e);
+    //};
+
+    const handleClick = (val: string) => {
+        setSelected(val);
     };
 
-    const mappedSeries = series.map((val, index) => {
+    //const mappedSeries = series.map((val, index) => {
+    //    return (
+    //        <label key={index}>
+    //            <input
+    //                type="radio"
+    //                value={val}
+    //                name="series"
+    //                checked={selected === val}
+    //                onChange={(e) => handleChange(e)}
+    //            />
+    //            {val}
+    //        </label>
+    //    );
+    //});
+
+    const boxSeries = series.map((val, index) => {
         return (
-            <label key={index}>
-                <input
-                    type="radio"
-                    value={val}
-                    name="series"
-                    checked={selected === val}
-                    onChange={(e) => handleChange(e)}
-                />
+            <div
+                className={`series click_box__div ${selected === val ? "selected" : ""}`}
+                onClick={() => handleClick(val)}
+                key={index}
+            >
                 {val}
-            </label>
+            </div>
         );
     });
     return (
         <section className="series select">
-            <div className="select__radios">{mappedSeries}</div>
+            <div className="select__radios">{boxSeries}</div>
             <BackNextButtons compNav={compNav} />
         </section>
     );
