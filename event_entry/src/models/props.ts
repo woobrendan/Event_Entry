e6f021dd90eb3a817695645dec1ecad4dc5c9838
Brendan Woo <woo.brendan@gmail.com>
2024-03-12 15:43:19 -0400

    create interface clickandnav

--- event_entry/src/models/props.ts
+++ event_entry/src/models/props.ts
@@ -2,3 +2,8 @@ export interface SetAndNav {
     compNav: (val: string) => void;
     handleFormElement: <T extends HTMLInputElement | HTMLSelectElement>(e: React.ChangeEvent<T>) => void;
 }
+
+export interface ClickAndNav {
+    compNav: (val: string) => void;
+    handleBoxClick: (name: string, val: string) => void;
+}
