98b850958e8f8fec824c44c15f366beb537360b5
Brendan Woo <woo.brendan@gmail.com>
2024-03-13 11:46:28 -0400

    setup template for class select component

---
+++ event_entry/src/components/event_entry/ClassSelect.tsx
@@ -0,0 +1,9 @@
+const ClassSelect: React.FC = () => {
+    return (
+        <section className="class_select_container click_component">
+            {/*<div className="click_box">{boxTickets}</div>*/}
+        </section>
+    );
+};
+
+export default ClassSelect;
