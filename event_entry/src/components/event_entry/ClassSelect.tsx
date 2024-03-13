a83bc3e7763d94a40a83e3513ebd32c1eed1988c
Brendan Woo <woo.brendan@gmail.com>
2024-03-13 13:03:02 -0400

    import backnext buttons component and implement. import interface for click box, and make interface props and decsontruct props

--- event_entry/src/components/event_entry/ClassSelect.tsx
+++ event_entry/src/components/event_entry/ClassSelect.tsx
@@ -1,7 +1,15 @@
-const ClassSelect: React.FC = () => {
+import { ClickAndNav } from "../../models/props";
+import BackNextButtons from "../BackNextButtons";
+
+interface Props extends ClickAndNav {
+    series: string;
+}
+
+const ClassSelect: React.FC<Props> = ({ series, handleBoxClick, compNav }) => {
     return (
         <section className="class_select_container click_component">
             {/*<div className="click_box">{boxTickets}</div>*/}
+            <BackNextButtons compNav={compNav} />
         </section>
     );
 };
