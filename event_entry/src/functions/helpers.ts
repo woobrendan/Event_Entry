cef11199098f1eaf7b139b66af8a87de53beb275
Brendan Woo <woo.brendan@gmail.com>
2024-03-13 13:03:37 -0400

    create functions folder, create helpers filewith getseries short name

---
+++ event_entry/src/functions/helpers.ts
@@ -0,0 +1,13 @@
+const getSeriesShort = (series: string): string => {
+    const seriesList: { [key: string]: string } = {
+        "Fanatec GT World Challenge America": "gtwc",
+        "Pirelli GT4 America": "gt4a",
+        "GT America": "gta",
+        "TC America": "tcam",
+        "Toyota GR Cup": "grCup",
+    };
+
+    return seriesList[series];
+};
+
+export { getSeriesShort };
