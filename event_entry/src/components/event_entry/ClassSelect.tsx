d1f15af268283f9e27b28791d5d620a3cde56fee
Brendan Woo <woo.brendan@gmail.com>
2024-03-14 13:37:59 -0400

    add usestae for selected, add in handleclikc function

--- event_entry/src/components/event_entry/ClassSelect.tsx
+++ event_entry/src/components/event_entry/ClassSelect.tsx
@@ -1,12 +1,14 @@
 import { ClickAndNav } from "../../models/props";
 import BackNextButtons from "../BackNextButtons";
 import { getSeriesShort } from "../../functions/helpers";
+import { useState } from "react";
 
 interface Props extends ClickAndNav {
     series: string;
 }
 
 const ClassSelect: React.FC<Props> = ({ series, handleBoxClick, compNav }) => {
+    const [selected, setSelected] = useState("");
     const classList: { [key: string]: string[] } = {
         gtwc: ["Pro", "Pro-Am", "Am"],
         gt4a: ["Silver", "Pro-Am", "Am"],
@@ -15,10 +17,15 @@ const ClassSelect: React.FC<Props> = ({ series, handleBoxClick, compNav }) => {
         grCup: ["Am"],
     };
 
+    const handleClick = (val: string) => {
+        setSelected(val);
+        handleBoxClick("class", val);
+    };
+
     const boxClass = classList[getSeriesShort(series)].map((classif, index) => {
         return (
             <div
-                className={`event click_box__div ${selected === classif ? "selected" : ""}`}
+                className={`classif click_box__div ${selected === classif ? "selected" : ""}`}
                 onClick={() => handleClick(classif)}
                 key={index}
             >
@@ -28,7 +35,7 @@ const ClassSelect: React.FC<Props> = ({ series, handleBoxClick, compNav }) => {
     });
     return (
         <section className="class_select_container click_component">
-            {/*<div className="click_box">{boxTickets}</div>*/}
+            <div className="click_box">{boxClass}</div>
             <BackNextButtons compNav={compNav} />
         </section>
     );
