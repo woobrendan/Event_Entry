d7b7dcefd63039f48508e3ffa1fe55f5bb03b3db
Brendan Woo <woo.brendan@gmail.com>
2024-03-14 13:47:49 -0400

    fix spelling for gt america

--- event_entry/src/functions/helpers.ts
+++ event_entry/src/functions/helpers.ts
@@ -2,7 +2,7 @@ const getSeriesShort = (series: string): string => {
     const seriesList: { [key: string]: string } = {
         "Fanatec GT World Challenge America": "gtwc",
         "Pirelli GT4 America": "gt4a",
-        "GT America": "gta",
+        "GT America": "gtam",
         "TC America": "tcam",
         "Toyota GR Cup": "grCup",
     };
