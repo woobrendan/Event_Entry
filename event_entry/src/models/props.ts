ac93c72a8f04429963cc766aad012416c4bb1dc2
Brendan Woo <woo.brendan@gmail.com>
2024-03-19 13:00:37 -0400

    create interface EventORder

--- event_entry/src/models/props.ts
+++ event_entry/src/models/props.ts
@@ -7,3 +7,12 @@ export interface ClickAndNav {
     compNav: (val: string) => void;
     handleBoxClick: (name: string, val: string) => void;
 }
+
+export interface EventOrder {
+    event: string;
+    series: string;
+    class: string;
+    number: string;
+    team: string;
+    sponsors: string;
+}
