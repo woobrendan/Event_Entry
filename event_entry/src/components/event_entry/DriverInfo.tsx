020de40e0bfac8fb890b8ac3d7ac4ba2e2ec1964
Brendan Woo <woo.brendan@gmail.com>
2024-03-20 16:53:34 -0400

    add state for driver, copy over handleselect

--- event_entry/src/components/event_entry/DriverInfo.tsx
+++ event_entry/src/components/event_entry/DriverInfo.tsx
@@ -1,6 +1,23 @@
 import { SetAndNav, DriverInfo } from "../../models/props";
+import { useState } from "react";
 
-const DriverInfo: React.FC<SetAndNav> = () => {
+const DriverInfo: React.FC<SetAndNav> = ({ compNav, handleFormElement }) => {
+    const [driver, setDriver] = useState({
+        driverName: "",
+        driverNAT: "",
+        fiaCAT: "",
+        hometown: "",
+        email: "",
+        cell: "",
+    });
+
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setDriver((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+        handleFormElement(e);
+    };
     return <></>;
 };
 
