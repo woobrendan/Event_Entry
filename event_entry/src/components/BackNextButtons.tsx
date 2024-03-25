52b13d89d866e7293f112e98a4523f7ea9dcf453
Brendan Woo <woo.brendan@gmail.com>
2024-03-25 17:22:38 -0400

    add to props isValid, deconstruct and pass to disabled prop for button component

--- event_entry/src/components/BackNextButtons.tsx
+++ event_entry/src/components/BackNextButtons.tsx
@@ -2,15 +2,16 @@ import { Button } from "@mui/material";
 
 interface Props {
     compNav: (val: string) => void;
+    isValid: boolean;
 }
 
-const BackNextButtons: React.FC<Props> = ({ compNav }) => {
+const BackNextButtons: React.FC<Props> = ({ compNav, isValid }) => {
     return (
         <div className="comp_nav_buttons">
             <Button variant="outlined" color="error" onClick={() => compNav("back")}>
                 Back
             </Button>
-            <Button variant="contained" color="error" onClick={() => compNav("next")}>
+            <Button disabled={!isValid} variant="contained" color="error" onClick={() => compNav("next")}>
                 Next
             </Button>
         </div>
