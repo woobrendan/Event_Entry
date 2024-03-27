4b7197b36a8f9e6ecc79cc316efc05fcd5a59287
Brendan Woo <woo.brendan@gmail.com>
2024-03-27 15:57:54 -0400

    create component to handle select for drivers, add string to oninput and make onchange call oninput

---
+++ event_entry/src/components/event_entry/DriverSelectElement.tsx
@@ -0,0 +1,39 @@
+interface Select {
+    label: string;
+    className: string;
+    name: string;
+    value: string;
+    onInput: (e: React.ChangeEvent<HTMLSelectElement>, num: string) => void;
+    valArr: string[];
+    driverNum: string;
+}
+
+const DriverSelectElements: React.FC<Select> = ({ label, className, name, value, onInput, valArr, driverNum }) => {
+    if (name === "driverName") {
+        valArr = valArr.sort();
+    }
+
+    return (
+        <div className={className}>
+            <label>{label}:</label>
+            <select
+                name={name}
+                value={value}
+                onInput={(e: React.ChangeEvent<HTMLSelectElement>) => onInput(e, driverNum)}
+            >
+                <option value="" disabled>
+                    Select {label}
+                </option>
+                {valArr.map((val, index) => {
+                    return (
+                        <option value={val} key={index}>
+                            {val}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+};
+
+export default DriverSelectElements;
