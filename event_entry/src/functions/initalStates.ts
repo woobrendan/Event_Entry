68bf64ca6241a2c24ef62cec601da60eac241889
Brendan Woo <woo.brendan@gmail.com>
2024-06-06 15:10:05 -0400

    create file to house initial states for ticket types

---
+++ event_entry/src/functions/initalStates.ts
@@ -0,0 +1,32 @@
+const initialEventOrder = {
+	type: "eventOrder",
+	event: "",
+	series: "",
+	class: "",
+	number: "",
+	team: "",
+	vehicle: "",
+	sponsors: "",
+	driver1: {
+		driverName: "",
+		driverNAT: "",
+		fiaCAT: "",
+		hometown: "",
+		email: "",
+		cell: "",
+	},
+};
+
+const initialBronze = {
+	type: "bronzeTest",
+	driver1: {
+		driverName: "",
+		driverNAT: "",
+		fiaCAT: "",
+		hometown: "",
+		email: "",
+		cell: "",
+	},
+};
+
+export { initialEventOrder, initialBronze };
