8db4a233da378398cf481365768e9eb4a94c23fe
Brendan Woo <woo.brendan@gmail.com>
2024-06-06 12:52:06 -0400

    adjust starting component to be overallorder instead of orderform

--- event_entry/src/App.tsx
+++ event_entry/src/App.tsx
@@ -1,12 +1,14 @@
 import OrderForm from "./components/OrderForm";
+import OverallOrder from "./components/OverallOrder";
 import "./styles/app.scss";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <OrderForm />
-        </div>
-    );
+	return (
+		<div className="App">
+			{/* <OrderForm /> */}
+			<OverallOrder />
+		</div>
+	);
 };
 
 export default App;
