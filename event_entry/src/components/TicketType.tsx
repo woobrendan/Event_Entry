1c26f1368e624264ffa61da1ccf07cf83c81ae37
Brendan Woo <woo.brendan@gmail.com>
2024-06-07 12:40:28 -0400

    update keys in ticketypes array to match interface name convetion, change click box logic to use settickettype

--- event_entry/src/components/TicketType.tsx
+++ event_entry/src/components/TicketType.tsx
@@ -1,42 +1,47 @@
 import { useState } from "react";
 import { ClickAndNav } from "../models/props";
 
-interface Props extends ClickAndNav {
-    type: string;
+interface Props {
+	compNav: (val: string) => void;
+	setTicketType: (val: string) => void;
+	type: string;
 }
 
-const TicketType: React.FC<Props> = ({ type = "", handleBoxClick, compNav }) => {
-    const [selected, setSelected] = useState(type);
+const TicketType: React.FC<Props> = ({ type = "", setTicketType, compNav }) => {
+	const [selected, setSelected] = useState(type);
 
-    const ticketTypes = [
-        { value: "event", label: "Event Entry" },
-        { value: "lumirank", label: "Lumirank Rental" },
-        { value: "bronzeTest", label: "Bronze Test Session" },
-    ];
+	const ticketTypes = [
+		{ value: "eventOrder", label: "Event Entry" },
+		{ value: "rental", label: "Lumirank Rental" },
+		{ value: "bronzeTest", label: "Bronze Test Session" },
+	];
 
-    const handleClick = (val: string) => {
-        setSelected(val);
-        handleBoxClick("type", val);
-        compNav("next");
-    };
+	const handleClick = (val: string) => {
+		setSelected(val);
+		// handleBoxClick("type", val);
+		setTicketType(val);
+		compNav("next");
+	};
 
-    const boxTickets = ticketTypes.map((ticket, index) => {
-        return (
-            <div
-                className={`ticket click_box__div ${selected === ticket.value ? "selected" : ""}`}
-                onClick={() => handleClick(ticket.value)}
-                key={index}
-            >
-                {ticket.label}
-            </div>
-        );
-    });
+	const boxTickets = ticketTypes.map((ticket, index) => {
+		return (
+			<div
+				className={`ticket click_box__div ${
+					selected === ticket.value ? "selected" : ""
+				}`}
+				onClick={() => handleClick(ticket.value)}
+				key={index}
+			>
+				{ticket.label}
+			</div>
+		);
+	});
 
-    return (
-        <section className="ticket_type_container click_component">
-            <div className="click_box">{boxTickets}</div>
-        </section>
-    );
+	return (
+		<section className="ticket_type_container click_component">
+			<div className="click_box">{boxTickets}</div>
+		</section>
+	);
 };
 
 export default TicketType;
