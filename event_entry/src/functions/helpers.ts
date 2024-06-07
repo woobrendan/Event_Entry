4de5397d444959a69b9ed9d504f7e8484430f0b3
Brendan Woo <woo.brendan@gmail.com>
2024-06-07 11:44:59 -0400

    add initialevent order key bronzeTest as false

--- event_entry/src/functions/helpers.ts
+++ event_entry/src/functions/helpers.ts
@@ -1,41 +1,44 @@
 const getSeriesShort = (series: string): string => {
-    const seriesList: { [key: string]: string } = {
-        "Fanatec GT World Challenge America": "gtwc",
-        "Pirelli GT4 America": "gt4a",
-        "GT America": "gtam",
-        "TC America": "tcam",
-        "Toyota GR Cup": "grCup",
-    };
+	const seriesList: { [key: string]: string } = {
+		"Fanatec GT World Challenge America": "gtwc",
+		"Pirelli GT4 America": "gt4a",
+		"GT America": "gtam",
+		"TC America": "tcam",
+		"Toyota GR Cup": "grCup",
+	};
 
-    return seriesList[series];
+	return seriesList[series];
 };
 
 const initialEventOrder = {
-    type: "",
-    event: "",
-    series: "",
-    class: "",
-    number: "",
-    team: "",
-    vehicle: "",
-    sponsors: "",
-    driver1: {
-        driverName: "",
-        driverNAT: "",
-        fiaCAT: "",
-        hometown: "",
-        email: "",
-        cell: "",
-    },
+	type: "",
+	event: "",
+	series: "",
+	class: "",
+	number: "",
+	team: "",
+	vehicle: "",
+	sponsors: "",
+	driver1: {
+		driverName: "",
+		driverNAT: "",
+		fiaCAT: "",
+		hometown: "",
+		email: "",
+		cell: "",
+	},
+	bronzeTest: {
+		isSelected: false,
+	},
 };
 
 const singleDriverObj = {
-    driverName: "",
-    driverNAT: "",
-    fiaCAT: "",
-    hometown: "",
-    email: "",
-    cell: "",
+	driverName: "",
+	driverNAT: "",
+	fiaCAT: "",
+	hometown: "",
+	email: "",
+	cell: "",
 };
 
 export { getSeriesShort, initialEventOrder, singleDriverObj };
