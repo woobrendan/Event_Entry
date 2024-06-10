78a5377ac8441a5c62d766fecd2d67a1c600051c
Brendan Woo <woo.brendan@gmail.com>
2024-06-10 16:20:30 -0400

    wrap component in roruter and routes, create path for each compoennt

--- event_entry/src/App.tsx
+++ event_entry/src/App.tsx
@@ -1,12 +1,18 @@
 import OverallOrder from "./components/OverallOrder";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./styles/app.scss";
+import EventOrderForm from "./components/event_entry/EventOrderForm";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			{/* <OrderForm /> */}
-			<OverallOrder />
-		</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<OverallOrder />} />
+					{/* <Route path="/eventOrder" element={<EventOrderForm />} /> */}
+				</Routes>
+			</div>
+		</Router>
 	);
 };
 
