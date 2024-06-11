5a5b657849d00f8c5b4a971d8c8f422dc6e7b4fc
Brendan Woo <woo.brendan@gmail.com>
2024-06-11 17:27:29 -0400

    add option for types to be empty string, for intial state

--- event_entry/src/store/types.ts
+++ event_entry/src/store/types.ts
@@ -2,7 +2,7 @@ import { EventOrder, BronzeTest } from "../models/props";
 
 export interface BaseTicket {
 	ticketNum: number;
-	type: "EventOrder" | "BronzeTest"; // add union of rental and test day
+	type: "EventOrder" | "BronzeTest" | ""; // add union of rental and test day
 }
 
 export type Ticket = (BaseTicket & EventOrder) | (BaseTicket & BronzeTest);
