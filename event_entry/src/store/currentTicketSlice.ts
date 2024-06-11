98b17bc21613ab3f7d21195452e0ebac980dfa60
Brendan Woo <woo.brendan@gmail.com>
2024-06-11 17:50:45 -0400

    create reducers for setticket type

--- event_entry/src/store/currentTicketSlice.ts
+++ event_entry/src/store/currentTicketSlice.ts
@@ -1,5 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Ticket } from "./types";
+import { initialEventOrder, initialBronzeTest } from "./initialStates";
+import { BaseTicket } from "./types";
+import { BronzeTest, EventOrder } from "../models/props";
 
 const initialState: Ticket = {
 	ticketNum: 1,
@@ -8,7 +11,26 @@ const initialState: Ticket = {
 const currentTicketSlice = createSlice({
 	name: "currentTicket",
 	initialState,
-	reducers: {},
+	reducers: {
+		setTicketType(
+			state: Ticket,
+			action: PayloadAction<"EventOrder" | "BronzeTest">
+		) {
+			const { payload: type } = action;
+
+			if (type === "EventOrder") {
+				return {
+					...state,
+					...initialEventOrder,
+				} as BaseTicket & Partial<EventOrder>;
+			} else if (type === "BronzeTest") {
+				return {
+					...state,
+					...initialBronzeTest,
+				} as BaseTicket & Partial<BronzeTest>;
+			}
+		},
+	},
 });
 
 export const currentTicketActions = currentTicketSlice.actions;
