de1463a4ff998645246bffc6a2b16b2f8e79e5f5
Brendan Woo <woo.brendan@gmail.com>
2024-06-12 13:36:41 -0400

    remmove set ticket type logic, remove old usestates of current tix, change components back to being static array

--- event_entry/src/components/OverallOrder.tsx
+++ event_entry/src/components/OverallOrder.tsx
@@ -1,63 +1,17 @@
 import { useState } from "react";
-import { Order, EventOrder, BronzeTest } from "../models/props";
-
-import { initialBronze, initialEventOrder } from "../functions/initalStates";
-
 import NewOrder from "./NewOrder";
 import TicketType from "./TicketType";
 
 import "../styles/orderForm.scss";
-import EventOrderForm from "./event_entry/EventOrderForm";
 
 const OverallOrder: React.FC = () => {
-	const [overallOrder, setOverallOrder] = useState<Order>({});
 	const [currentComp, setCurrentComp] = useState(0);
-	const [currentTicket, setCurrentTicket] = useState(1);
 
 	const compNav = (val: string): void => {
-		if (val === "next" || val === "back") {
-			setCurrentComp((prev) => (val === "next" ? prev + 1 : prev - 1));
-		}
-		if (val === "restart") {
-			setCurrentComp(1);
-		}
+		setCurrentComp((prev) => (val === "next" ? prev + 1 : prev - 1));
 	};
 
-	// Handle Logic for setting ticket type. set as ticket x in overall order, then set intial state
-	const setTicketType = (name: string) => {
-		const currentTix = `ticket${currentTicket}`;
-		let initial: EventOrder | BronzeTest;
-
-		if (name === "eventOrder") {
-			setOverallOrder((prev) => ({
-				...prev,
-				[currentTix]: initialEventOrder as EventOrder,
-			}));
-
-			const newCompList = [
-				...components,
-				<EventOrderForm compNav={compNav} />,
-			];
-
-			setComponents(newCompList);
-		} else if (name === "bronzeTest") {
-			setOverallOrder((prev) => ({
-				...prev,
-				[currentTix]: initialBronze as BronzeTest,
-			}));
-		}
-	};
-
-	const thisTicket = Object.keys(overallOrder)[0];
-
-	const [components, setComponents] = useState([
-		<NewOrder compNav={compNav} />,
-		<TicketType
-			setTicketType={setTicketType}
-			compNav={compNav}
-			type={thisTicket}
-		/>,
-	]);
+	const components = [<NewOrder compNav={compNav} />, <TicketType />];
 
 	const currentComponent = components[currentComp];
 	return (
