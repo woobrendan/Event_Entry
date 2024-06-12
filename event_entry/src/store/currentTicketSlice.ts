fe47db59d80fb50a20e920c9cd21902402633e14
Brendan Woo <woo.brendan@gmail.com>
2024-06-12 17:14:41 -0400

    Create handleEventForm reducer to handle form events

--- event_entry/src/store/currentTicketSlice.ts
+++ event_entry/src/store/currentTicketSlice.ts
@@ -1,12 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Ticket } from "./types";
 import { initialEventOrder, initialBronzeTest } from "./initialStates";
-import {
-	BaseTicket,
-	isEventOrder,
-	EventOrder,
-	DriverInfoInterface,
-} from "./types";
+import { BaseTicket, isEventOrder, EventOrder, DriverInfoInterface } from "./types";
 // import { BronzeTest, EventOrder } from "../models/props";
 
 interface ClickValues {
@@ -36,10 +31,7 @@ const currentTicketSlice = createSlice({
 				} as Ticket;
 			}
 		},
-		setEventKeyValue(
-			state,
-			action: PayloadAction<{ [key: string]: string }>
-		) {
+		setEventKeyValue(state, action: PayloadAction<{ [key: string]: string }>) {
 			const { name, value }: ClickValues = action.payload;
 
 			if (isEventOrder(state)) {
@@ -48,6 +40,32 @@ const currentTicketSlice = createSlice({
 					(copyState as any)[name] = value;
 				}
 				return copyState;
+			}
+		},
+		handleEventForm(state, action: PayloadAction<{ driver: string | undefined; name: string; value: string }>) {
+			const { driver, name, value } = action.payload;
+
+			if (isEventOrder(state)) {
+				const copyState: EventOrder = {
+					...state,
+					...(state.driver1 ? state.driver1 : {}),
+					...(state.driver2 ? state.driver2 : {}),
+				};
+
+				if (driver === "driver1" || driver === "driver2") {
+					// Update driver info in currentTicket
+					copyState[driver] = {
+						...(copyState[driver] as DriverInfoInterface),
+						[name]: value,
+					};
+					return copyState;
+				} else {
+					// Update other properties in currentTicket
+					return {
+						...copyState,
+						[name]: value,
+					};
+				}
 			}
 		},
 	},
