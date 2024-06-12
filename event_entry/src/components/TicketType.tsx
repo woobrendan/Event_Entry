25998e43c5d4a0ca9815ef14a9b25e764ea79f3f
Brendan Woo <woo.brendan@gmail.com>
2024-06-12 18:13:12 -0400

    import dispatch and implement, then dispatch action in handleclick

--- event_entry/src/components/TicketType.tsx
+++ event_entry/src/components/TicketType.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { ClickAndNav } from "../models/props";
+import { useAppDispatch } from "../store/hooks";
+import { currentTicketActions } from "../store/currentTicketSlice";
 
 interface Props {
 	compNav: (val: string) => void;
@@ -10,6 +12,8 @@ interface Props {
 const TicketType: React.FC<Props> = ({ type = "", setTicketType, compNav }) => {
 	const [selected, setSelected] = useState(type);
 
+	const dispatch = useAppDispatch();
+
 	const ticketTypes = [
 		{ value: "eventOrder", label: "Event Entry" },
 		{ value: "rental", label: "Lumirank Rental" },
@@ -18,27 +22,28 @@ const TicketType: React.FC<Props> = ({ type = "", setTicketType, compNav }) => {
 
 	const handleClick = (val: string) => {
 		setSelected(val);
-		setTicketType(val);
+		// setTicketType(val);
+		dispatch(currentTicketActions.setTicketType(val));
 		compNav("next");
 	};
 
-	const boxTickets = ticketTypes.map((ticket, index) => {
-		return (
-			<div
-				className={`ticket click_box__div ${
-					selected === ticket.value ? "selected" : ""
-				}`}
-				onClick={() => handleClick(ticket.value)}
-				key={index}
-			>
-				{ticket.label}
-			</div>
-		);
-	});
-
 	return (
 		<section className="ticket_type_container click_component">
-			<div className="click_box">{boxTickets}</div>
+			<div className="click_box">
+				{ticketTypes.map((ticket, index) => {
+					return (
+						<div
+							className={`ticket click_box__div ${
+								selected === ticket.value ? "selected" : ""
+							}`}
+							onClick={() => handleClick(ticket.value)}
+							key={index}
+						>
+							{ticket.label}
+						</div>
+					);
+				})}
+			</div>
 		</section>
 	);
 };
