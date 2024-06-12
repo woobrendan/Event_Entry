11651918e6a061b56d2bc3e15a17632d3526ef7c
Brendan Woo <woo.brendan@gmail.com>
2024-06-12 14:17:22 -0400

    add in dirverinterface, event order and bronze test, and add iseventorder type guard

--- event_entry/src/store/types.ts
+++ event_entry/src/store/types.ts
@@ -1,4 +1,4 @@
-import { EventOrder, BronzeTest } from "../models/props";
+// import { EventOrder, BronzeTest } from "../models/props";
 
 export interface BaseTicket {
 	ticketNum: number;
@@ -6,6 +6,41 @@ export interface BaseTicket {
 	type: string;
 }
 
-export type Ticket =
-	| (BaseTicket & Partial<EventOrder>)
-	| (BaseTicket & Partial<BronzeTest>);
+interface DriverInfoInterface {
+	driverName: string;
+	driverNAT: string;
+	fiaCAT: string;
+	hometown: string;
+	email: string;
+	cell: string;
+}
+
+export interface EventOrder extends BaseTicket {
+	type: string;
+	event: string;
+	series: string;
+	class: string;
+	number: string;
+	team: string;
+	sponsors: string;
+	vehicle: string;
+	driver1: DriverInfoInterface;
+	driver2?: DriverInfoInterface;
+}
+
+interface BronzeTest extends BaseTicket {
+	type: string;
+	isSelected?: boolean;
+	driver1?: DriverInfoInterface;
+	driver2?: DriverInfoInterface;
+}
+
+// export type Ticket =
+// 	| (BaseTicket & Partial<EventOrder>)
+// 	| (BaseTicket & Partial<BronzeTest>);
+
+export type Ticket = BaseTicket & (Partial<EventOrder> | Partial<BronzeTest>);
+
+export const isEventOrder = (ticket: Ticket): ticket is EventOrder => {
+	return (ticket as EventOrder).event !== undefined;
+};
