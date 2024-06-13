ed296f1672cd0627dcbce7c8922cfa85e8b8b220
Brendan Woo <woo.brendan@gmail.com>
2024-06-13 17:36:44 -0400

    lint file

--- event_entry/src/components/event_entry/EntryInfo.tsx
+++ event_entry/src/components/event_entry/EntryInfo.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { SetAndNav, EventOrder } from "../../models/props";
+import { SetAndNav } from "../../models/props";
+import { EventOrder } from "../../store/types";
 import { teams } from "../../seeds/teams";
 import BackNextButtons from "../BackNextButtons";
 import vehicles from "../../seeds/vehicles";
@@ -7,98 +8,93 @@ import { getSeriesShort } from "../../functions/helpers";
 import SelectElements from "./SelectElements";
 
 interface Props extends SetAndNav {
-    eventOrder: EventOrder;
+	eventOrder: EventOrder;
 }
 
 const EntryInfo: React.FC<Props> = ({ compNav, handleFormElement, eventOrder }) => {
-    const { team = "", number = "", vehicle = "", sponsors = "" } = eventOrder;
+	const { team = "", number = "", vehicle = "", sponsors = "" } = eventOrder;
 
-    const [details, setDetails] = useState({
-        number,
-        team,
-        vehicle,
-        sponsors,
-    });
+	const [details, setDetails] = useState({ number, team, vehicle, sponsors });
 
-    const series = getSeriesShort(eventOrder.series);
+	const series = getSeriesShort(eventOrder.series);
 
-    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setDetails((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-        handleFormElement(e);
-    };
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setDetails((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+		handleFormElement(e);
+	};
 
-    const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setDetails((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-        handleFormElement(e);
-    };
+	const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setDetails((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+		handleFormElement(e);
+	};
 
-    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setDetails((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-        handleFormElement(e);
-    };
+	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setDetails((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+		handleFormElement(e);
+	};
 
-    const getVehicleArr = (series: string, classif: string): string[] => {
-        const seriesData = vehicles[series];
-        if (Array.isArray(seriesData)) {
-            return seriesData;
-        } else {
-            const classVehicles = seriesData[classif];
-            return classVehicles;
-        }
-    };
+	const getVehicleArr = (series: string, classif: string): string[] => {
+		const seriesData = vehicles[series];
+		if (Array.isArray(seriesData)) {
+			return seriesData;
+		} else {
+			const classVehicles = seriesData[classif];
+			return classVehicles;
+		}
+	};
 
-    const isValid = [details.number, details.team, details.vehicle].every((value) => Boolean(value));
+	const isValid = [details.number, details.team, details.vehicle].every((value) => Boolean(value));
 
-    return (
-        <section className="team_info input">
-            <div className="input__team">
-                <label>Car Number:</label>
-                <input
-                    className="input__team__number "
-                    type="number"
-                    value={details.number}
-                    name="number"
-                    onInput={handleInput}
-                />
-            </div>
-            <SelectElements
-                label="Team"
-                className="input__team"
-                name="team"
-                value={details.team}
-                onInput={handleSelect}
-                valArr={teams}
-            />
-            <SelectElements
-                label="Vehicle"
-                className="input__team"
-                name="vehicle"
-                value={details.vehicle}
-                onInput={handleSelect}
-                valArr={getVehicleArr(series, eventOrder.class)}
-            />
-            <div className=" sponsors">
-                <label>Sponsors (Please separate sponsors with comma):</label>
-                <textarea
-                    rows={4}
-                    cols={50}
-                    value={details.sponsors}
-                    name="sponsors"
-                    onChange={handleTextArea}
-                ></textarea>
-            </div>
-            <BackNextButtons compNav={compNav} isValid={isValid} />
-        </section>
-    );
+	return (
+		<section className="team_info input">
+			<div className="input__team">
+				<label>Car Number:</label>
+				<input
+					className="input__team__number "
+					type="number"
+					value={details.number}
+					name="number"
+					onInput={handleInput}
+				/>
+			</div>
+			<SelectElements
+				label="Team"
+				className="input__team"
+				name="team"
+				value={details.team}
+				onInput={handleSelect}
+				valArr={teams}
+			/>
+			<SelectElements
+				label="Vehicle"
+				className="input__team"
+				name="vehicle"
+				value={details.vehicle}
+				onInput={handleSelect}
+				valArr={getVehicleArr(series, eventOrder.class)}
+			/>
+			<div className=" sponsors">
+				<label>Sponsors (Please separate sponsors with comma):</label>
+				<textarea
+					rows={4}
+					cols={50}
+					value={details.sponsors}
+					name="sponsors"
+					onChange={handleTextArea}
+				></textarea>
+			</div>
+			<BackNextButtons compNav={compNav} isValid={isValid} />
+		</section>
+	);
 };
 
 export default EntryInfo;
