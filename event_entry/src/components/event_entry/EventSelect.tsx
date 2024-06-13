381cc09f43cd11e70c1c61842831c8d1559dc035
Brendan Woo <woo.brendan@gmail.com>
2024-06-13 17:33:44 -0400

    remove boxevents variable and directly map through events inside component return

--- event_entry/src/components/event_entry/EventSelect.tsx
+++ event_entry/src/components/event_entry/EventSelect.tsx
@@ -3,47 +3,47 @@ import BackNextButtons from "../BackNextButtons";
 import { ClickAndNav } from "../../models/props";
 
 interface Props extends ClickAndNav {
-    event: string;
+	event: string;
 }
 
 const EventSelect: React.FC<Props> = ({ compNav, handleBoxClick, event }) => {
-    const [selected, setSelected] = useState(event ? event : "");
+	const [selected, setSelected] = useState(event ? event : "");
 
-    const events = [
-        "Full Season Entry",
-        "Sonoma Raceway",
-        "Long Beach Grand Prix",
-        "Sebring International Raceway",
-        "Circuit of the Americas",
-        "VIRginia International Raceway",
-        "Road America",
-        "Barber Motorsports Park",
-        "Indianpolis Motor Speedway",
-    ];
+	const events = [
+		"Full Season Entry",
+		"Sonoma Raceway",
+		"Long Beach Grand Prix",
+		"Sebring International Raceway",
+		"Circuit of the Americas",
+		"VIRginia International Raceway",
+		"Road America",
+		"Barber Motorsports Park",
+		"Indianpolis Motor Speedway",
+	];
 
-    const handleClick = (val: string) => {
-        setSelected(val);
-        handleBoxClick("event", val);
-    };
+	const handleClick = (val: string) => {
+		setSelected(val);
+		handleBoxClick("event", val);
+	};
 
-    const boxEvents = events.map((event, index) => {
-        return (
-            <div
-                className={`event click_box__div ${selected === event ? "selected" : ""}`}
-                onClick={() => handleClick(event)}
-                key={index}
-            >
-                {event}
-            </div>
-        );
-    });
-
-    return (
-        <section className="event_container click_component">
-            <div className="click_box click_box__event">{boxEvents}</div>
-            <BackNextButtons compNav={compNav} isValid={selected ? true : false} />
-        </section>
-    );
+	return (
+		<section className="event_container click_component">
+			<div className="click_box click_box__event">
+				{events.map((event, index) => {
+					return (
+						<div
+							className={`event click_box__div ${selected === event ? "selected" : ""}`}
+							onClick={() => handleClick(event)}
+							key={index}
+						>
+							{event}
+						</div>
+					);
+				})}
+			</div>
+			<BackNextButtons compNav={compNav} isValid={selected ? true : false} />
+		</section>
+	);
 };
 
 export default EventSelect;
