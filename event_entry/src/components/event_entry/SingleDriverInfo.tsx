8a3c81f0ffba58f73d982b9513ffef9a366a90a2
Brendan Woo <woo.brendan@gmail.com>
2024-06-14 17:33:30 -0400

    import drivers and country codes seeds, add to props driver entry, handle select and input

--- event_entry/src/components/event_entry/SingleDriverInfo.tsx
+++ event_entry/src/components/event_entry/SingleDriverInfo.tsx
@@ -1,11 +1,19 @@
 import DriverSelectElement from "./DriverSelectElement";
+import { DriverObjInterface } from "../../models/props";
+import drivers from "../../seeds/drivers";
+import countryCodes from "../../seeds/countryCodes";
 
 interface Props {
 	driverNum: string;
+	driverEntry: DriverObjInterface;
+	handleSelect: (e: React.ChangeEvent<HTMLSelectElement>, driverNum: string) => void;
+	handleInput: (e: React.ChangeEvent<HTMLInputElement>, driverNum: string) => void;
 }
 
-const SingleDriverInfo: React.FC<Props> = ({ driverNum }) => {
+const SingleDriverInfo: React.FC<Props> = ({ driverNum, driverEntry, handleSelect, handleInput }) => {
+	const driver = driverEntry[`driver${driverNum}` as keyof typeof driverEntry];
 	const { driverName, driverNAT, fiaCAT, hometown, cell, email } = driver;
+
 	return (
 		<div className="driver_info__driver">
 			<DriverSelectElement
@@ -61,13 +69,13 @@ const SingleDriverInfo: React.FC<Props> = ({ driverNum }) => {
 					onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e, driverNum)}
 				/>
 			</div>
-			<div>
+			{/* <div>
 				<input
 					type="checkbox"
 					onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleCheck(e, driverNum)}
 					checked={isChecked[`check${driverNum}` as keyof isChecked]}
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 };
