501ec4588c6575217c82db0a57c2482da9a4d858
Brendan Woo <woo.brendan@gmail.com>
2024-06-17 17:47:37 -0400

    update imports and remove single driver info variable

--- event_entry/src/components/event_entry/DriverInfo.tsx
+++ event_entry/src/components/event_entry/DriverInfo.tsx
@@ -1,11 +1,8 @@
-import { SetAndNav, DriverObjInterface } from "../../models/props";
-import { EventOrder } from "../../store/types";
+import { SetAndNav } from "../../models/props";
+import { EventOrder, DriverObjInterface } from "../../store/types";
 import { useState, useEffect } from "react";
-import drivers from "../../seeds/drivers";
-import countryCodes from "../../seeds/countryCodes";
 import BackNextButtons from "../BackNextButtons";
-import DriverSelectElement from "./DriverSelectElement";
-import { getSeriesShort, singleDriverObj } from "../../functions/helpers";
+import { getSeriesShort } from "../../functions/helpers";
 import SingleDriverInfo from "./SingleDriverInfo";
 
 interface Props extends SetAndNav {
@@ -58,77 +55,6 @@ const DriverInfo: React.FC<Props> = ({ compNav, handleFormElement, eventOrder })
 	//         [driver]:
 	//     }))
 	// }
-
-	const singleDriverInfo = (driverNum: string) => {
-		const driver = driverEntry[`driver${driverNum}` as keyof typeof driverEntry];
-		if (driver) {
-			const { driverName, driverNAT, fiaCAT, hometown, cell, email } = driver;
-			return (
-				<div className="driver_info__driver">
-					<DriverSelectElement
-						label={`Driver ${driverNum}`}
-						className="input__driver"
-						name="driverName"
-						value={driverName}
-						onInput={handleSelect}
-						valArr={drivers}
-						driverNum={driverNum}
-					/>
-					<DriverSelectElement
-						label="Nationality"
-						className="input__driver"
-						name="driverNAT"
-						value={driverNAT}
-						onInput={handleSelect}
-						valArr={countryCodes}
-						driverNum={driverNum}
-					/>
-					<DriverSelectElement
-						label="FIA Rating"
-						className="input__driver"
-						name="fiaCAT"
-						value={fiaCAT}
-						onInput={handleSelect}
-						valArr={["N/A", "Bronze", "Silver", "Gold", "Platinum"]}
-						driverNum={driverNum}
-					/>
-					<div className="input__team">
-						<label>Driver Hometown:</label>
-						<input
-							value={hometown}
-							name="hometown"
-							onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e, driverNum)}
-						/>
-					</div>
-					<div className="input__team">
-						<label>Driver Email:</label>
-						<input
-							type="email"
-							value={email}
-							name="email"
-							onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e, driverNum)}
-						/>
-					</div>
-					<div className="input__team">
-						<label>Driver Cell:</label>
-						<input
-							type="tel"
-							value={cell}
-							name="cell"
-							onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e, driverNum)}
-						/>
-					</div>
-					{/* <div>
-						<input
-							type="checkbox"
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleCheck(e, driverNum)}
-							checked={isChecked[`check${driverNum}` as keyof isChecked]}
-						/>
-					</div> */}
-				</div>
-			);
-		}
-	};
 
 	return (
 		<section className="driver_info input">
