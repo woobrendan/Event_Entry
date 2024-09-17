4138e6b0e7dcf5e54a4b0488473be08d6b3fd1b2
Brendan Woo <woo.brendan@gmail.com>
2024-09-17 14:58:11 -0700

    add inputs for number and select for events, create function handlers for handleinput

--- event_entry/src/components/rentals/Lumirank.tsx
+++ event_entry/src/components/rentals/Lumirank.tsx
@@ -1,9 +1,58 @@
 import { seriesList } from "../../seeds/series";
+import { events } from "../../seeds/events";
 import { useState } from "react";
+import SelectElements from "../event_entry/SelectElements";
 
 const Lumirank: React.FC = () => {
-	const [rental, setRental] = useState({});
-	return <section></section>;
+	const [rental, setRental] = useState({
+		series: "",
+		event: "",
+		number: 0,
+	});
+
+	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setRental((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setRental((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+	return (
+		<section>
+			<SelectElements
+				label="Series"
+				className="input__series"
+				name="series"
+				value={rental.series}
+				onInput={handleSelect}
+				valArr={seriesList}
+			/>
+			<SelectElements
+				label="Event"
+				className="input__events"
+				name="events"
+				value={rental.event}
+				onInput={handleSelect}
+				valArr={events}
+			/>
+			<div className="input__number">
+				<label>Car Number:</label>
+				<input
+					className="input__number__rental"
+					type="number"
+					value={rental.number}
+					name="number"
+					onInput={handleInput}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default Lumirank;
