ff3da4425583b724b5cb57b137ee1eb8c62f64d2
Brendan Woo <woo.brendan@gmail.com>
2024-09-17 14:57:05 -0700

    Create a file to house events list

---
+++ event_entry/src/seeds/events.ts
@@ -0,0 +1,13 @@
+const events = [
+	"Full Season Entry",
+	"Sonoma Raceway",
+	"Long Beach Grand Prix",
+	"Sebring International Raceway",
+	"Circuit of the Americas",
+	"VIRginia International Raceway",
+	"Road America",
+	"Barber Motorsports Park",
+	"Indianpolis Motor Speedway",
+];
+
+export { events };
