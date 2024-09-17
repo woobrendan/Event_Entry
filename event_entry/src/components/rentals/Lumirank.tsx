8fc259f62595ceae950f1d63c721686bcec060c4
Brendan Woo <woo.brendan@gmail.com>
2024-09-17 14:13:45 -0700

    create rentals folder then pass in lumirank component

---
+++ event_entry/src/components/rentals/Lumirank.tsx
@@ -0,0 +1,9 @@
+import { seriesList } from "../../seeds/series";
+import { useState } from "react";
+
+const Lumirank: React.FC = () => {
+	const [rental, setRental] = useState({});
+	return <section></section>;
+};
+
+export default Lumirank;
