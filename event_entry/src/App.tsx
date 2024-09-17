667beae05489cc2c7451b337524dd25b55788c16
Brendan Woo <woo.brendan@gmail.com>
2024-09-17 14:38:11 -0700

    create new route for rentals, pass in lumirank component

--- event_entry/src/App.tsx
+++ event_entry/src/App.tsx
@@ -2,6 +2,9 @@ import OverallOrder from "./components/OverallOrder";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./styles/app.scss";
 import EventOrderForm from "./components/event_entry/EventOrderForm";
+import Lumirank from "./components/rentals/Lumirank";
+
+import "./styles/orderForm.scss";
 
 const App: React.FC = () => {
 	return (
@@ -10,6 +13,7 @@ const App: React.FC = () => {
 				<Routes>
 					<Route path="/" element={<OverallOrder />} />
 					<Route path="/eventOrder" element={<EventOrderForm />} />
+					<Route path="/rentals" element={<Lumirank />} />
 				</Routes>
 			</div>
 		</Router>
