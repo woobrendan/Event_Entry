b254a4d0596d2e315db83898eb12822cdb8e3ddf
Brendan Woo <woo.brendan@gmail.com>
2024-09-18 13:55:52 -0700

    give section classname rental container and wrap select elements in div team info

--- event_entry/src/components/rentals/Lumirank.tsx
+++ event_entry/src/components/rentals/Lumirank.tsx
@@ -8,12 +8,16 @@ const Lumirank: React.FC = () => {
 		series: "",
 		event: "",
 		number: 0,
+		cost: 0,
 	});
 
 	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const series = e.target.value;
+
 		setRental((prev) => ({
 			...prev,
-			[e.target.name]: e.target.value,
+			[e.target.name]: series,
+			cost: series === "Fanatec GT World Challenge America" ? 395 : 285,
 		}));
 	};
 
@@ -24,33 +28,38 @@ const Lumirank: React.FC = () => {
 		}));
 	};
 	return (
-		<section>
-			<SelectElements
-				label="Series"
-				className="input__series"
-				name="series"
-				value={rental.series}
-				onInput={handleSelect}
-				valArr={seriesList}
-			/>
-			<SelectElements
-				label="Event"
-				className="input__events"
-				name="events"
-				value={rental.event}
-				onInput={handleSelect}
-				valArr={events}
-			/>
-			<div className="input__number">
-				<label>Car Number:</label>
-				<input
-					className="input__number__rental"
-					type="number"
-					value={rental.number}
-					name="number"
-					onInput={handleInput}
+		<section className="rental_container">
+			<div className="team_info">
+				<SelectElements
+					label="Series"
+					className="input__series"
+					name="series"
+					value={rental.series}
+					onInput={handleSelect}
+					valArr={seriesList}
 				/>
+				<SelectElements
+					label="Event"
+					className="input__events"
+					name="events"
+					value={rental.event}
+					onInput={handleSelect}
+					valArr={events}
+				/>
+				<div className="input__number">
+					<label>Car Number:</label>
+					<input
+						className="input__number__rental"
+						type="number"
+						value={rental.number}
+						name="number"
+						onInput={handleInput}
+					/>
+				</div>
 			</div>
+			<h3>Check if you need to purchase</h3>
+			<input type="checkbox" />
+			<label>DID Cable</label>
 		</section>
 	);
 };
