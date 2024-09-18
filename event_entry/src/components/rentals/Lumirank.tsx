3940f0707b6afe687df4e0eca6a696b775fb5a45
Brendan Woo <woo.brendan@gmail.com>
2024-09-18 16:58:31 -0700

    pass variables to input element, add logic inside handleinput to update state

--- event_entry/src/components/rentals/Lumirank.tsx
+++ event_entry/src/components/rentals/Lumirank.tsx
@@ -5,13 +5,25 @@ import SelectElements from "../event_entry/SelectElements";
 import "../../styles/rental.scss";
 
 const Lumirank: React.FC = () => {
-	const [rental, setRental] = useState({
+	const [rental, setRental] = useState<{ [key: string]: any }>({
 		series: "",
 		event: "",
 		number: 0,
+		lrCable: false,
+		didCable: false,
+		gpsCable: false,
+		canCable: false,
 		cost: 0,
 	});
 
+	const cableCosts: { [key: string]: number } = {
+		lrCable: 35,
+		didCable: 35,
+		gpsCable: 190,
+		canCable: 170,
+	};
+
+	// handle select elements to set the series value, event value and cost
 	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		if (e.target.name === "series") {
 			const series = e.target.value;
@@ -29,11 +41,26 @@ const Lumirank: React.FC = () => {
 	};
 
 	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, type } = e.target;
+
+		if (type === "checkbox") {
+			let cost = rental.cost;
+			let newBool = !rental[name];
+
+			newBool ? (cost += cableCosts[name]) : (cost -= cableCosts[name]);
+
+			setRental((prev) => ({
+				...prev,
+				[name]: newBool,
+				cost,
+			}));
+		}
 		setRental((prev) => ({
 			...prev,
 			[e.target.name]: e.target.value,
 		}));
 	};
+
 	return (
 		<section className="rental_container">
 			<div className="team_info">
@@ -67,7 +94,7 @@ const Lumirank: React.FC = () => {
 			<h3>Check if you need to purchase power</h3>
 			<div className="rental__checkbox_container">
 				<div className="cable_checkbox">
-					<input type="checkbox" />
+					<input type="checkbox" name="lrCable" checked={rental.lrCable} onChange={handleInput} />
 					<label>Lumirank Cable ($35)</label>
 				</div>
 				<div className="cable_checkbox">
