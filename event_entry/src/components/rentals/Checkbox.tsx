8fcbdb2a29baca12cca03e2e565e456c8a5a9dbb
Brendan Woo <woo.brendan@gmail.com>
2024-09-19 13:38:49 -0700

    add to props onchange, checked and name. deconstruct those values in props and pass to input

--- event_entry/src/components/rentals/Checkbox.tsx
+++ event_entry/src/components/rentals/Checkbox.tsx
@@ -1,11 +1,14 @@
 interface Props {
 	label: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	checked: boolean;
+	name: string;
 }
 
-const CheckBox: React.FC<Props> = ({ label }) => {
+const CheckBox: React.FC<Props> = ({ label, onChange, checked, name }) => {
 	return (
 		<div className="cable_checkbox">
-			<input type="checkbox" />
+			<input type="checkbox" name={name} onChange={onChange} checked={checked} />
 			<label>{label}</label>
 		</div>
 	);
