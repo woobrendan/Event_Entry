0bf7b2a1150658f909ef24bb3edf848a4e19f369
Brendan Woo <woo.brendan@gmail.com>
2024-09-20 13:01:04 -0700

    add in tickettpye component and pass to root path, add in ticket type styling

--- event_entry/src/App.tsx
+++ event_entry/src/App.tsx
@@ -1,17 +1,18 @@
-import OverallOrder from "./components/OverallOrder";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./styles/app.scss";
 import EventOrderForm from "./components/event_entry/EventOrderForm";
 import Lumirank from "./components/rentals/Lumirank";
+import TicketType from "./components/TicketType";
 
 import "./styles/orderForm.scss";
+import "./styles/ticketType.scss";
 
 const App: React.FC = () => {
 	return (
 		<Router>
 			<div className="App">
 				<Routes>
-					<Route path="/" element={<OverallOrder />} />
+					<Route path="/" element={<TicketType />} />
 					<Route path="/eventOrder" element={<EventOrderForm />} />
 					<Route path="/rentals" element={<Lumirank />} />
 				</Routes>
