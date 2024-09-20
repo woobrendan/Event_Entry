b35036ad78a67ae208b5258f23836560f02149e6
Brendan Woo <woo.brendan@gmail.com>
2024-09-20 13:01:30 -0700

    remove altapp component and put app component back in:

--- event_entry/src/index.tsx
+++ event_entry/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import AppAlt from "./AppAlt";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -11,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<AppAlt />
+			<App />
 		</Provider>
 	</React.StrictMode>
 );
