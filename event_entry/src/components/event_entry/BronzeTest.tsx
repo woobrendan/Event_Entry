4db655ecbc571509cfa3cd2322d7fd4ca28dff0e
Brendan Woo <woo.brendan@gmail.com>
2024-09-20 13:03:50 -0700

    import setand nav and event order. comment out buttsona dn map through events

--- event_entry/src/components/event_entry/BronzeTest.tsx
+++ event_entry/src/components/event_entry/BronzeTest.tsx
@@ -1,4 +1,5 @@
-import { EventOrder, SetAndNav } from "../../models/props";
+import { SetAndNav } from "../../models/props";
+import { EventOrder } from "../../store/types";
 
 interface Props extends SetAndNav {
 	eventOrder: EventOrder;
@@ -6,8 +7,26 @@ interface Props extends SetAndNav {
 
 const BronzeTest: React.FC<Props> = ({ eventOrder }) => {
 	const { driver1, series } = eventOrder;
-	console.log("series", series);
-	return <section></section>;
+
+	return (
+		<section className="event_container click_component">
+			{/* <div className="click_box click_box__event">
+				<h1>Bronze Test Session</h1>
+				{events.map((event, index) => {
+					return (
+						<div
+							className={`event click_box__div ${selected === event ? "selected" : ""}`}
+							onClick={() => handleClick(event)}
+							key={index}
+						>
+							{event}
+						</div>
+					);
+				})}
+			</div>
+			<BackNextButtons compNav={compNav} isValid={selected ? true : false} /> */}
+		</section>
+	);
 };
 
 export default BronzeTest;
