dc3650cf38ac17262d54d0082ac8629dc36a9b87
Brendan Woo <woo.brendan@gmail.com>
2024-11-13 08:30:38 -0600

    import useappselector and pass to table bbody to loop through the values of tickets and pass to imported component of ticketlistitem

--- event_entry/src/components/Cart/CartDashboard.tsx
+++ event_entry/src/components/Cart/CartDashboard.tsx
@@ -1,5 +1,29 @@
+import { useAppSelector } from "../../store/hooks";
+import TicketListItem from "./TicketListItem";
+
 const CartDashboard: React.FC = () => {
-	return <section className="dashboard"></section>;
+	const tickets = useAppSelector((state) => state.ticketList.ticketList);
+	return (
+		<section className="dashboard">
+			<table>
+				<thead>
+					<tr>
+						<th>Ticket Type</th>
+						<th>Event</th>
+						<th>Quantity</th>
+						<th>Cost</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tickets.map((ticket, index) => (
+						<TicketListItem ticketItem={ticket} key={index} />
+						// add on delete
+					))}
+				</tbody>
+			</table>
+		</section>
+	);
 };
 
 export default CartDashboard;
