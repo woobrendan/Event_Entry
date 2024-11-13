151996f3b65b1f53be1db73941d0ab426ea29596
Brendan Woo <woo.brendan@gmail.com>
2024-11-13 08:29:49 -0600

    create interface Props and pass to component. deconstruct values from ticket and pass into component of tr elements

--- event_entry/src/components/Cart/TicketListItem.tsx
+++ event_entry/src/components/Cart/TicketListItem.tsx
@@ -1,5 +1,22 @@
-const TicketListItem: React.FC = () => {
-	return <></>;
+import { BaseTicket } from "../../store/types";
+
+interface Props {
+	ticketItem: BaseTicket;
+}
+
+const TicketListItem: React.FC<Props> = ({ ticketItem }) => {
+	console.log("ticket", ticketItem);
+	const { event, ticketType } = ticketItem;
+	// const ticketType = ticketItem.ticketType === "Rental" ? "Lumirank Rental" : ""
+	// cost, quantity, onDelete
+	return (
+		<tr>
+			<td>{ticketType}</td>
+			<td>{event}</td>
+			{/* <td>{cost}</td> */}
+			{/* <td>q{uantity}</td> */}
+		</tr>
+	);
 };
 
 export default TicketListItem;
