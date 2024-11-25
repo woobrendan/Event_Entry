235d65ccdddaa33c04644dd9abf3f4b7eb3c30ad
Brendan Woo <woo.brendan@gmail.com>
2024-11-25 14:13:36 -0800

    replace basic table row elements with material uielements. create delete btton

--- event_entry/src/components/Cart/TicketListItem.tsx
+++ event_entry/src/components/Cart/TicketListItem.tsx
@@ -1,21 +1,33 @@
-import { BaseTicket } from "../../store/types";
+import { Ticket } from "../../store/types";
+import { TableRow, TableCell, Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface Props {
-	ticketItem: BaseTicket;
+	ticketItem: Ticket;
+	ticketIndex: number;
 }
 
-const TicketListItem: React.FC<Props> = ({ ticketItem }) => {
+const TicketListItem: React.FC<Props> = ({ ticketItem, ticketIndex }) => {
 	console.log("ticket", ticketItem);
-	const { event, ticketType } = ticketItem;
+	let { event, ticketType, cost } = ticketItem;
 	// const ticketType = ticketItem.ticketType === "Rental" ? "Lumirank Rental" : ""
 	// cost, quantity, onDelete
+
+	if (ticketType === "Rental") {
+		ticketType = "Lumirank Rental";
+	}
+
+	// on delete take in ticketindex and go to store and delete
 	return (
-		<tr>
-			<td>{ticketType}</td>
-			<td>{event}</td>
-			{/* <td>{cost}</td> */}
-			{/* <td>q{uantity}</td> */}
-		</tr>
+		<TableRow>
+			<TableCell>{ticketType}</TableCell>
+			<TableCell>{event}</TableCell>
+			<TableCell align="right">{cost}</TableCell>
+			<TableCell align="right">Details TBD</TableCell>
+			<TableCell align="right">
+				<Button className="deleteTicket" variant="contained" color="error" startIcon={<DeleteIcon />} />
+			</TableCell>
+		</TableRow>
 	);
 };
 
