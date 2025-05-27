6754f5e8818a1c18912962ec629decd162b603ed
Brendan Woo <woo.brendan@gmail.com>
2025-05-27 12:47:47 -0700

    add logic to handle showing the navbar or not

--- event_entry/src/App.tsx
+++ event_entry/src/App.tsx
@@ -1,26 +1,55 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router, useLocation } from "react-router-dom";
 import "./styles/app.scss";
 import EventOrderForm from "./components/event_entry/EventOrderForm";
 import Lumirank from "./components/rentals/Lumirank";
 import TicketType from "./components/TicketType";
 import CartDashboard from "./components/Cart/CartDashboard";
 import BronzeTest from "./components/bronze_test/BronzeTest";
+import NavBar from "./components/NavBar";
 
 import "./styles/orderForm.scss";
 import "./styles/ticketType.scss";
 
+// const App: React.FC = () => {
+// 	return (
+// 		<Router>
+// 			<div className="App">
+// 				<Routes>
+// 					<Route path="/" element={<TicketType />} />
+// 					<Route path="/eventOrder" element={<EventOrderForm />} />
+// 					<Route path="/rentals" element={<Lumirank />} />
+// 					<Route path="/cart" element={<CartDashboard />} />
+// 					<Route path="/bronzeTest" element={<BronzeTest />} />
+// 				</Routes>
+// 			</div>
+// 		</Router>
+// 	);
+// };
+
+const AppContent: React.FC = () => {
+	const location = useLocation();
+	const hideNavBarOn = ["/"]; // List of routes to hide the NavBar
+
+	const shouldShowNavBar = !hideNavBarOn.includes(location.pathname);
+
+	return (
+		<div className="App">
+			{shouldShowNavBar && <NavBar />}
+			<Routes>
+				<Route path="/" element={<TicketType />} />
+				<Route path="/eventOrder" element={<EventOrderForm />} />
+				<Route path="/rentals" element={<Lumirank />} />
+				<Route path="/cart" element={<CartDashboard />} />
+				<Route path="/bronzeTest" element={<BronzeTest />} />
+			</Routes>
+		</div>
+	);
+};
+
 const App: React.FC = () => {
 	return (
 		<Router>
-			<div className="App">
-				<Routes>
-					<Route path="/" element={<TicketType />} />
-					<Route path="/eventOrder" element={<EventOrderForm />} />
-					<Route path="/rentals" element={<Lumirank />} />
-					<Route path="/cart" element={<CartDashboard />} />
-					<Route path="/bronzeTest" element={<BronzeTest />} />
-				</Routes>
-			</div>
+			<AppContent />
 		</Router>
 	);
 };
