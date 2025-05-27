fd2c24c61ab910b79b1cf73b41a04b9b9bf77254
Brendan Woo <woo.brendan@gmail.com>
2025-05-27 12:05:51 -0700

    add logic for removeTicket

--- event_entry/src/store/ticketListSlice.ts
+++ event_entry/src/store/ticketListSlice.ts
@@ -14,9 +14,13 @@ const ticketListSlice = createSlice({
 		addTicket(state, action: PayloadAction<Ticket>) {
 			state.ticketList = [...state.ticketList, action.payload];
 		},
-		removeTicket(state, action: PayloadAction<any>) {
-			const prevState = [...state.ticketList];
-			//fitler out by ticket number
+		removeTicket(state, action: PayloadAction<number>) {
+			const indexToRemove = action.payload;
+
+			// check that the index val is within the range of the array
+			if (indexToRemove >= 0 && indexToRemove < state.ticketList.length) {
+				state.ticketList.splice(indexToRemove, 1);
+			}
 		},
 	},
 });
